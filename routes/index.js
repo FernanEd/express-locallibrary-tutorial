e45009da4fa09bc1dd3bf311ba6185b19f94ceda
FernanEd <fernandoed.bm@gmail.com>
2021-03-25 18:49:07 -0600

    initial

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+var book_controller = require("../controllers/bookController");
+
+/* GET home page. */
+router.get("/", book_controller.index);
+
+module.exports = router;
